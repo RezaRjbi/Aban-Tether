103dcd5d386eefc000846be37fddef066e238eaa
Reza Rajabi <rza.rajabi@gmail.com>
2025-02-26 22:04:43 +0330

    add deposit view

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("balance/", views.BalanceView.as_view(), name="balance")]
+urlpatterns = [
+    path("balance/", views.BalanceView.as_view(), name="balance"),
+    path("statement/", views.TransactionView.as_view(), name="transactions"),
+    path("deposit/", views.DepositView.as_view(), name="deposit"),
+]
