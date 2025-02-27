d734876a0884c7c6cc97f0f5a04a98e520e304e0
Reza Rajabi <rza.rajabi@gmail.com>
2025-02-27 12:22:00 +0330

    add a view for exchange operations

--- exchanges/serializers.py
+++ exchanges/serializers.py
@@ -7,3 +7,11 @@ class ExchangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exchange
         exclude = ["id", "user"]
+        read_only_fields = [
+            "currency",
+            "fee",
+            "quantity",
+            "type",
+            "created_at",
+            "updated_at",
+        ]
--- exchanges/views.py
+++ exchanges/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 
 from .models import Exchange
 from .serializers import ExchangeSerializer
@@ -10,3 +11,21 @@ class ExchangeList(generics.ListAPIView):
 
     def get_queryset(self):
         return Exchange.objects.filter(user=self.request.user)
+
+
+class ExchangeRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ExchangeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    @staticmethod
+    def is_update_allowed(exchange: Exchange, raise_: bool = True) -> bool:
+        allowed = exchange.state == Exchange.State.PENDING
+        if not allowed and raise_:
+            raise ValidationError("only pending exchanges could be cancelled")
+        return allowed
+
+    def get_object(self) -> Exchange:
+        instance = super().get_object().filter(user=self.request.user)
+        if self.request.method not in permissions.SAFE_METHODS:
+            self.is_update_allowed(instance, raise_=True)
+        return instance
